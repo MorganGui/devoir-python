267221e1ad6935d9a58535beb4b1a530dcf08995
MorganGui <morgan.guillemin1@gmail.com>
2023-11-03 16:36:04 +0100

    ff classes

--- classes/Episode.py
+++ classes/Episode.py
@@ -1,10 +1,12 @@
 class Episode:
-  def __init__(self, id, name, episode, season, date, country, channel, url):
-    self.id = id
-    self.name = name
-    self.episode = episode
-    self.season = season
-    self.date = date
-    self.country = country
-    self.channel = channel
-    self.url = url
+  # def __init__(self, id, name, episode, season, date, country, channel, url):
+  #   self.id = id
+  #   self.name = name
+  #   self.episode = episode
+  #   self.season = season
+  #   self.date = date
+  #   self.country = country
+  #   self.channel = channel
+  #   self.url = url
+  def __init__(self):
+    self.test = 'ok'
