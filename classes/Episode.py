55d189f35aa26c4812f0ca77dc583d42d7323130
MorganGui <morgan.guillemin1@gmail.com>
2023-11-03 16:19:27 +0100

    try classes

---
+++ classes/Episode.py
@@ -0,0 +1,10 @@
+class Episode:
+  def __init__(self, id, name, episode, season, date, country, channel, url):
+    self.id = id
+    self.name = name
+    self.episode = episode
+    self.season = season
+    self.date = date
+    self.country = country
+    self.channel = channel
+    self.url = url
